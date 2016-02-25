056209e1e05f55dff109fba3b1f12a342203dd54
Stefan Wille <github@stefanwille.com>
2016-02-25 12:02:18 +0100

    Update default size

--- src/redux/modules/board.js
+++ src/redux/modules/board.js
@@ -1,5 +1,5 @@
-export const INITIAL_WIDTH = 30
-export const INITIAL_HEIGHT = 30
+export const INITIAL_WIDTH = 40
+export const INITIAL_HEIGHT = 20
 
 const setCellActionHandler = (state, action) => {
   const {x, y, live} = action.payload
